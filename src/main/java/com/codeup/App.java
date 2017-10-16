b43f6fd7fa880a03331200d461fe39f538ee01ee
brandongossen <brandongossen@yahoo.com>
2017-10-16 10:00:56 -0500

    Initial commit

---
+++ src/main/java/com/codeup/App.java
@@ -0,0 +1,21 @@
+package com.codeup;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+
+        boolean isBlank = StringUtils.isBlank("     ");
+        System.out.println(isBlank);
+    }
+
+//    public static void main( String[] args )
+//    {
+//        System.out.println( "Hello World!" );
+//    }
+}
